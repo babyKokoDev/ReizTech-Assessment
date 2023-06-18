85ed4f6fc6480a7c66e67840c7773370df6ecc8f
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-06-18 15:43:57 +0100

    tables

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,8 +6,12 @@ interface Props {
 
 const Table = (props: Props) => {
     return (
-        <div className='text-white'>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, repudiandae?
+        <div className='text-white w-3/5 h-[550px] mx-auto border-solid border-gray-600 border-[1px] rounded-xl flex flex-col justify-between'>
+              <div className='h-[45px] bg-[#374151] rounded-t-lg'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, dolorem.
+              </div>
+              <div className=''></div>
+              <div className='h-[50px] bg-[#374151] rounded-b-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, quidem!</div>
         </div>
     )
 }
